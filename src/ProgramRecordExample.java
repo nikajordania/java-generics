e0e86c78ff2b834506e36b49eed95d1360a0d6cb
Nika Zhordania <nika.zhordania.1@gmail.com>
2023-12-09 04:05:48 +0400

    update: ProgramRecordExample

--- src/ProgramRecordExample.java
+++ src/ProgramRecordExample.java
@@ -55,3 +55,45 @@ class Person {
 
 record PersonRecord(String name, int age) {
 }
+
+record PersonChecked(String name, int age) {
+
+    PersonChecked {
+
+        if (age < 1 || age > 110) {
+            age = 18;
+        }
+
+    }
+
+//    PersonChecked(String name, int age){
+//
+//        if(age < 0 || age > 120) age = 18;
+//
+//        this.name = name;
+//        this.age = age;
+//    }
+
+    PersonChecked(String firstName, String lastName, int age) {
+
+        this(firstName + " " + lastName, age);
+
+    }
+
+    // Override
+    public String name() {
+        return "Mister " + name;
+    }
+
+    public String toString() {
+        return String.format("Person %s, Age: %d", name, age);
+    }
+
+
+    static int minAge;
+
+    static {
+        minAge = 18;
+        System.out.println("Static initializer");
+    }
+}
