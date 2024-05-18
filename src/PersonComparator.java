4814d019356e675fea9b88a537489e6ec7cdc59c
Nika Zhordania <nika.zhordania.1@gmail.com>
2024-05-18 11:04:26 +0400

    update

---
+++ src/PersonComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        return Integer.compare(p1.age(), p2.age());
+    }
+}
